168ae6f4084396790ea5f5cabf21396324817c05
EC2 Default User <ec2-user@ip-172-31-62-102.ec2.internal>
2023-03-10 16:40:26 +0000

    Configuration changes made from AWS Cloud-9 IDE

--- create.sh
+++ create.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=$4
+aws cloudformation create-stack --stack-name $1 --template-body file://$2  --parameters file://$3 --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM" --region=us-east-1
